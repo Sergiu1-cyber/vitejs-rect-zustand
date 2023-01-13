a1e890e193fb4217581af791e545c9d839625896
Sergiu1-cyber <68728921+Sergiu1-cyber@users.noreply.github.com>
2023-01-13 13:14:18 +0200

    add zustand

--- src/components/Counter.jsx
+++ src/components/Counter.jsx
@@ -2,13 +2,19 @@ import { useCounterStore } from '../store/counter';
 
 export default function Counter() {
   function Display() {
-    const counter = useCounterStore((store) => store.counter);
+    const counter = useCounterStore((state) => state.counter);
     return <div>Counter: {counter}</div>;
+  }
+
+  function Increment() {
+    const increment = useCounterStore((state) => state.counterIncrement);
+    return <button onClick={increment}>increment</button>;
   }
 
   return (
     <div>
       <Display />
+      <Increment />
     </div>
   );
 }
