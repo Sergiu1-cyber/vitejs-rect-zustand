010b584d5dee278cf06c51acc80c1714c717a3fe
Sergiu1-cyber <68728921+Sergiu1-cyber@users.noreply.github.com>
2023-01-13 12:44:14 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# vitejs-rect-zustand
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/vitejs-vite-cueac8)
---
+++ src/store/counter.js
@@ -0,0 +1,10 @@
+import {create} from 'zustand'
+
+export const useCounterStore = create(
+  (set) => ({
+    counter: 0,
+    counterIncrement: () => set(
+      (state) => ({counter: state.counter + 1})
+    )
+  }),
+)
